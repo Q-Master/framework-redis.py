ac1c78b50c1bd70307ae12308aacdfacb889b02f
Vladimir Berezenko <qmaster2000@gmail.com>
2023-03-27 17:37:31 +0300

    Fix for newer build system

--- asyncframework/db/redis/__init__.py
+++ asyncframework/db/redis/__init__.py
@@ -8,3 +8,17 @@ from script_field import *
 from script import *
 from set_field import *
 from set import *
+
+__version__ = '0.1.1'
+
+__title__ = 'asyncframework-redis'
+__description__ = 'Async framework redis addon.'
+__url__ = 'https://github.com/Q-Master/framework-redis.py'
+__uri__ = __url__
+__doc__ = f"{__description__} <{__uri__}>"
+
+__author__ = 'Vladimir Berezenko'
+__email__ = 'qmaster2000@gmail.com'
+
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2019-2023 Vladimir Berezenko'
