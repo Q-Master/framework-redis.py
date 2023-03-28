f62431cbe0a415454fd1754b24a6d345b30efc9f
Vladimir Berezenko <qmaster2000@gmail.com>
2023-03-28 13:39:13 +0300

    Import fix

--- asyncframework/db/redis/__init__.py
+++ asyncframework/db/redis/__init__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 from .connection import *
-from lock_field import *
-from lock import *
-from record_field import *
-from record import *
-from script_field import *
-from script import *
-from set_field import *
-from set import *
+from .lock_field import *
+from .lock import *
+from .record_field import *
+from .record import *
+from .script_field import *
+from .script import *
+from .set_field import *
+from .set import *
 
 __version__ = '0.1.1'
 
